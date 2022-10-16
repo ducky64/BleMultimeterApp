5990c1e5f7239c63be92c1b2ea3f7f4d321d31ed
Richard Lin <elpato25@gmail.com>
2022-10-16 16:53:21 -0700

    scan results

--- app/src/main/java/com/example/blemultimeterapp/MainActivity.java
+++ app/src/main/java/com/example/blemultimeterapp/MainActivity.java
@@ -4,33 +4,48 @@ import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.util.Log;
 import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
+import androidx.wear.widget.WearableLinearLayoutManager;
 
 import com.example.blemultimeterapp.databinding.ActivityMainBinding;
-import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
 
 // from this tutorial: https://punchthrough.com/android-ble-guide/
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
-    private Button mScanButton;
     private ActivityMainBinding binding;
 
     private final static int kEnableBluetoothRequestCode = 1;
     private final static int kPermissionBluetoothRequestCode = 2;
     private final static int kPermissionLocationRequestCode = 3;
-    private BluetoothAdapter bluetoothAdapter = null;
+    private BluetoothAdapter btAdapter = null;
+    private Optional<BluetoothLeScanner> btScanner = Optional.empty();
+
+    private List<ScanResult> scanResults = new ArrayList<>();
+    private ScanResultAdapter scanResultAdapter = new ScanResultAdapter(scanResults, result -> {
+        Toast.makeText(MainActivity.this.getApplicationContext(), result.getDevice().getName(), Toast.LENGTH_SHORT)
+                .show();
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,22 +54,24 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
-        mScanButton = binding.scanButton;
-        mScanButton.setOnClickListener(view -> startBleScan());
+        binding.scanResults.setEdgeItemsCenteringEnabled(true);
+        binding.scanResults.setLayoutManager(new WearableLinearLayoutManager(this));
+        binding.scanResults.setAdapter(scanResultAdapter);
+
+        binding.scanButton.setOnClickListener(view -> startBleScan());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (bluetoothAdapter == null) {
-            bluetoothAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
+        if (btAdapter == null) {
+            btAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
         }
         startup();
     }
 
     protected void startup() {
-        if (!bluetoothAdapter.isEnabled()) {
+        if (!btAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent((BluetoothAdapter.ACTION_REQUEST_ENABLE));
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S &&
                     ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
@@ -92,7 +109,47 @@ public class MainActivity extends Activity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, kPermissionLocationRequestCode);
             return;
         }
-        Toast.makeText(this.getApplicationContext(), "Starting BLE Scan", Toast.LENGTH_SHORT)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this.getApplicationContext(), "BLE scan permission required", Toast.LENGTH_SHORT)
+                    .show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_SCAN}, kPermissionLocationRequestCode);
+            return;
+        }
+
+        Toast.makeText(MainActivity.this.getApplicationContext(), "BLE Scan Start", Toast.LENGTH_SHORT)
                 .show();
+
+        ScanFilter filter = new ScanFilter.Builder()
+//                .setDeviceName("DuckyMultimeter")
+                .build();
+        BluetoothLeScanner scanner = btAdapter.getBluetoothLeScanner();
+        btScanner = Optional.of(scanner);
+        ScanSettings settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .build();
+
+        scanResults.clear();
+        scanResultAdapter.notifyDataSetChanged();
+        scanner.startScan(Arrays.asList(filter), settings, new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+                Log.i("Scan", "Scan found " + result.getDevice().getName());
+
+                OptionalInt index = IntStream.range(0, scanResults.size())
+                        .filter(i -> scanResults.get(i).getDevice().getAddress().equals(result.getDevice().getAddress()))
+                        .findFirst();
+                if (index.isPresent()) {  // update existing device
+                    scanResults.set(index.getAsInt(), result);
+                    scanResultAdapter.notifyItemChanged(index.getAsInt());
+                    Log.i("Scan", "Notify changed " + index.getAsInt());
+                } else {  // add new device
+                    scanResults.add(result);
+                    scanResultAdapter.notifyItemInserted(scanResults.size() - 1);
+                    Log.i("Scan", "Notify insert " + (scanResults.size() - 1));
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/blemultimeterapp/ScanResultAdapter.java
@@ -0,0 +1,61 @@
+package com.example.blemultimeterapp;
+
+import android.bluetooth.le.ScanResult;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.blemultimeterapp.databinding.RowScanResultBinding;
+
+import java.util.List;
+
+
+public class ScanResultAdapter extends RecyclerView.Adapter<ScanResultAdapter.ViewHolder> {
+    final private List<ScanResult> items;
+    final private ClickAction onClick;
+
+    public ScanResultAdapter(List<ScanResult> items, ClickAction onClick) {
+        this.items = items;
+        this.onClick = onClick;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RowScanResultBinding view = RowScanResultBinding.inflate(LayoutInflater.from(parent.getContext()));
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    interface ClickAction {
+        void action(ScanResult result);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        final private RowScanResultBinding itemView;
+        public ViewHolder(@NonNull RowScanResultBinding itemView) {
+            super(itemView.getRoot());
+            this.itemView = itemView;
+        }
+
+        void bind(ScanResult result) {
+            itemView.deviceName.setText(result.getDevice().getName());
+            itemView.macAddress.setText(result.getDevice().getAddress());
+            itemView.signalStrength.setText(result.getRssi() + " dBm");
+            itemView.getRoot().setOnClickListener(view -> {
+               ScanResultAdapter.this.onClick.action(result);
+            });
+        }
+    }
+}
