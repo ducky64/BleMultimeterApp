8efa6fbc899803c5c6d94df373c7a74d0d14a9a9
Richard Lin <elpato25@gmail.com>
2022-10-22 17:54:40 -0700

    starting to refactor to Nordic API

--- app/src/main/java/com/example/blemultimeterapp/MainActivity.java
+++ app/src/main/java/com/example/blemultimeterapp/MainActivity.java
@@ -5,37 +5,43 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.ScanResult;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.ParcelUuid;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.blemultimeterapp.databinding.ActivityMainBinding;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
 
+import no.nordicsemi.android.ble.BleManager;
+
 
 class MultimeterService {
-    public static ParcelUuid applyUuidOffset(ParcelUuid uuid, int offset) {
-        long msb = uuid.getUuid().getMostSignificantBits();
-        long lsb = uuid.getUuid().getLeastSignificantBits();
-        return new ParcelUuid(new UUID(msb + ((long)offset << 32) , lsb));
+    public static UUID applyUuidOffset(UUID uuid, int offset) {
+        long msb = uuid.getMostSignificantBits();
+        long lsb = uuid.getLeastSignificantBits();
+        return new UUID(msb + ((long)offset << 32) , lsb);
     }
 
-    public static ParcelUuid kUuidService = ParcelUuid.fromString("0bde0a01-cdb0-46bd-a3a2-85b7e16329d1");  // custom protocol
+    public static UUID kUuidService = UUID.fromString("0bde0a01-cdb0-46bd-a3a2-85b7e16329d1");  // custom protocol
 
     // Characteristics
-    public static ParcelUuid kUuidReading = applyUuidOffset(kUuidService, 1);
-    public static ParcelUuid kUuidMode = applyUuidOffset(kUuidService, 2);
-    public static ParcelUuid kUuidAdc = applyUuidOffset(kUuidService, 3);
-    public static ParcelUuid kUuidResistance = applyUuidOffset(kUuidService, 4);
+    public static UUID kUuidReading = applyUuidOffset(kUuidService, 1);
+    public static UUID kUuidMode = applyUuidOffset(kUuidService, 2);
+    public static UUID kUuidAdc = applyUuidOffset(kUuidService, 3);
+    public static UUID kUuidResistance = applyUuidOffset(kUuidService, 4);
 }
 
 
@@ -47,8 +53,9 @@ public class MainActivity extends FragmentActivity {
 
     public static int kGattMaxMtuSize = 517;  // defined in Android source, https://punchthrough.com/android-ble-guide/
 
-    public static ParcelUuid kUuidNus = ParcelUuid.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
-    public static ParcelUuid kUuidDeviceInfo = ParcelUuid.fromString("0000180a-0000-1000-8000-00805f9b34fb");
+    public static UUID kUuidCccDescriptor = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+    public static UUID kUuidNus = UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+    public static UUID kUuidDeviceInfo = UUID.fromString("0000180a-0000-1000-8000-00805f9b34fb");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +78,38 @@ public class MainActivity extends FragmentActivity {
         });
     }
 
+    // use Nordic's BLE library since Android's library is full of wtf
+    class MultimeterBleManager extends BleManager {
+        public MultimeterBleManager(@NonNull Context context) {
+            super(context);
+        }
+
+        @NonNull
+        @Override
+        protected BleManagerGattCallback getGattCallback() {
+            return new GattCallbackImpl();
+        }
+
+        private class GattCallbackImpl extends BleManagerGattCallback {
+            @Override
+            protected boolean isRequiredServiceSupported(@NonNull BluetoothGatt gatt) {
+                return false;
+            }
+
+            @Override
+            protected void initialize() {
+
+            }
+
+            @Override
+            protected void onServicesInvalidated() {
+
+            }
+        }
+    }
+
+
+
     private BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
         @Override
         @SuppressLint("MissingPermission")  // BLUETOOTH_CONNECT permission handled in ScanResultFragment
@@ -81,10 +120,7 @@ public class MainActivity extends FragmentActivity {
                 connectedGatt = Optional.of(gatt);
 
                 runOnUiThread(() -> {
-                    binding.connectProgress.setVisibility(View.INVISIBLE);
                     binding.deviceName.setText(gatt.getDevice().getName());
-                    binding.deviceReading.setText("-");  // initialize blank reading
-                    binding.deviceReading.setVisibility(View.VISIBLE);
                 });
 
                 // running on UI thread recommended to avoid potential deadlock: https://punchthrough.com/android-ble-guide/
@@ -115,18 +151,75 @@ public class MainActivity extends FragmentActivity {
         }
 
         @Override
+        @SuppressLint("MissingPermission")  // BLUETOOTH_CONNECT permission handled in ScanResultFragment
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             super.onServicesDiscovered(gatt, status);
-
-            Log.i("BluetoothGattCallback", "Expected VRead " + MultimeterService.kUuidReading);
-
             for (BluetoothGattService service : gatt.getServices()) {
                 Log.i("BluetoothGattCallback", "Discovered service " + service.getUuid());
                 for (BluetoothGattCharacteristic characteristic : service.getCharacteristics()) {
                     Log.i("BluetoothGattCallback", "  with characteristic " + characteristic.getUuid());
                 }
-
             }
+            BluetoothGattService multimeterService = gatt.getService(MultimeterService.kUuidService);
+            if (multimeterService == null) {
+                Log.e("BluetoothGattCallback", "Missing GATT service");
+                runOnUiThread(() -> {
+                    Toast.makeText(MainActivity.this.getApplicationContext(),
+                                    "Missing GATT service", Toast.LENGTH_SHORT)
+                            .show();
+                });
+                return;
+            }
+            BluetoothGattCharacteristic readingChar = multimeterService.getCharacteristic(MultimeterService.kUuidReading);
+            BluetoothGattCharacteristic modeChar = multimeterService.getCharacteristic(MultimeterService.kUuidMode);
+            BluetoothGattCharacteristic adcChar = multimeterService.getCharacteristic(MultimeterService.kUuidAdc);
+            BluetoothGattCharacteristic resistanceChar = multimeterService.getCharacteristic(MultimeterService.kUuidResistance);
+            if (readingChar == null || modeChar == null || adcChar == null || resistanceChar == null) {
+                Log.e("BluetoothGattCallback", "Missing GATT characteristics");
+                runOnUiThread(() -> {
+                    Toast.makeText(MainActivity.this.getApplicationContext(),
+                                    "Missing GATT characteristics", Toast.LENGTH_SHORT)
+                            .show();
+                });
+                return;
+            }
+
+            if (!gatt.setCharacteristicNotification(readingChar, true)
+                    || !gatt.setCharacteristicNotification(adcChar, true)
+                    || !gatt.setCharacteristicNotification(modeChar, true)
+                    || !gatt.setCharacteristicNotification(resistanceChar, true)) {
+                Log.e("BluetoothGattCallback", "Can't set notifications");
+                runOnUiThread(() -> {
+                    Toast.makeText(MainActivity.this.getApplicationContext(),
+                                    "Can't set notifications", Toast.LENGTH_SHORT)
+                            .show();
+                });
+                return;
+            }
+            BluetoothGattDescriptor descriptor;
+            descriptor = readingChar.getDescriptor(kUuidCccDescriptor);
+            descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+            gatt.writeDescriptor(descriptor);
+
+            descriptor = adcChar.getDescriptor(kUuidCccDescriptor);
+            descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+            gatt.writeDescriptor(descriptor);
+
+            descriptor = modeChar.getDescriptor(kUuidCccDescriptor);
+            descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+            gatt.writeDescriptor(descriptor);
+
+            runOnUiThread(() -> {
+                binding.connectProgress.setVisibility(View.INVISIBLE);
+                binding.deviceReading.setText("-");  // initialize blank reading
+                binding.deviceReading.setVisibility(View.VISIBLE);
+            });
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+            Log.i("BluetoothGattCallback", "Characteristic changed " + characteristic.getUuid() + " -> " + Arrays.toString(characteristic.getValue()));
         }
     };
 
